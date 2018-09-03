f5d968faed330078fe206d5e1af0494cf2f6aa36
Brande George <george.brande@gmail.com>
2018-09-03 19:08:04 +0300

    finished

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { FormGroup, FormControl, Validators, FormArray } from '@angular/forms';
+import { Observable } from 'rxjs/Observable';
 
 @Component({
   selector: 'app-root',
@@ -15,11 +16,26 @@ export class AppComponent implements OnInit{
     this.signupForm = new FormGroup({
       'userData': new FormGroup({
         'username': new FormControl(null,[Validators.required, this.forbiddenNames.bind(this)]),
-        'email' : new FormControl(null, [Validators.required, Validators.email])
+        'email' : new FormControl(null, [Validators.required, Validators.email], 
+        this.forbiddenEmails)
       }),
 
       'gender': new FormControl('male'),
       'hobbies':new FormArray([])
+    });
+    // this.signupForm.valueChanges.subscribe(
+    //   (value) => console.log(value)
+    // );
+    this.signupForm.statusChanges.subscribe(
+      (value) => console.log(value)
+    );
+    this.signupForm.setValue({
+      'userData': {
+        'username': 'Max',
+        'email': 'max@tet.com'
+      },
+      'gender':'male',
+      'hobbies': []  
     });
   }
 
@@ -40,5 +56,16 @@ export class AppComponent implements OnInit{
     return null;
   }
 
-
+  forbiddenEmails(control: FormControl): Promise<any> | Observable<any> {
+    const promise = new Promise<any>((resolve, reject) => {
+      setTimeout(() => {
+        if (control.value === 'test@test.com') {
+          resolve({'emailIsForbidden': true});
+        } else {
+          resolve(null);
+        }
+      }, 1500);
+    });
+    return promise;
+  }
 }
